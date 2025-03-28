eb9838ce8d2f06d7454849784202311ccdffbd16
Karthik N <karthikk.n.edu@gmail.com>
2025-03-28 15:10:57 +0530

    commit 2

--- Tech_Olympics/src/App.jsx
+++ Tech_Olympics/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter,Outlet,Route,Routes } from 'react-router'
-
+import Home from './pages/home'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,7 +11,7 @@ function App() {
     <>
   <BrowserRouter>
   <Routes>
-    <Route path='/' element={<p >hello world</p>}>
+    <Route path='/' element={<Home/>}>
       
     </Route>
   </Routes>
